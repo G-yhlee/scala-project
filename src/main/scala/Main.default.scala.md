6275e815f10b26930548be47a60bd502c0eda85a
G-yhlee <yhleesoft@gmail.com>
2022-12-09 17:54:21 +0900

    run fizzbuzz

---
+++ src/main/scala/Main.default.scala.md
@@ -0,0 +1,7 @@
+object Main extends App {
+  // val test = Left[Int, Int](2)
+  // println(test.fold[Int](a => a * a, a => a))
+
+  println(HelloWorld.run)
+
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,22 @@
 import cats.effect.{IO, IOApp}
-
-object HelloWorld extends IOApp.Simple {
-  val run = IO.println("Hello, World!")
-
-}
-
-// object Main extends App {
-//   // val test = Left[Int, Int](2)
-//   // println(test.fold[Int](a => a * a, a => a))
-
-//   println(HelloWorld.run)
+import scala.concurrent.duration._
+// object HelloWorld extends IOApp.Simple {
+//   val run = IO.println("Hello, World!")
 
 // }
+
+object StupidFizzBuzz extends IOApp.Simple {
+  val run =
+    for {
+      ctr <- IO.ref(0)
+
+      wait = IO.sleep(1.second)
+      poll = wait *> ctr.get
+
+      _ <- poll.flatMap(IO.println(_)).foreverM.start
+      _ <- poll.map(_ % 3 == 0).ifM(IO.println("fizz"), IO.unit).foreverM.start
+      _ <- poll.map(_ % 5 == 0).ifM(IO.println("buzz"), IO.unit).foreverM.start
+
+      _ <- (wait *> ctr.update(_ + 1)).foreverM.void
+    } yield ()
+}
